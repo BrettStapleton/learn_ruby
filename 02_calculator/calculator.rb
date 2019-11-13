1faae66be8c27ad580a9960e31a735b134647a16
BrettStapleton <brett.i.stapleton@gmail.com>
2019-11-13 15:07:08 -0600

    Add tests. Pass tests.

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,33 @@
 #write your code here
+def add(num1, num2)
+    num1 + num2
+end
+
+def subtract(num1, num2)
+    num1 - num2
+end
+
+def sum(arr)
+    total = 0
+    arr.each { |a| total += a }
+    total
+end
+
+def multiply(arr)
+    total = 1
+    arr.each { |a| total *= a }
+    total
+end
+
+def power(num1, exponent)
+    num1 ** exponent
+end
+
+def factorial(num)
+    total = 1
+    while (num > 0)
+        total *= num
+        num -= 1
+    end
+    total
+end
